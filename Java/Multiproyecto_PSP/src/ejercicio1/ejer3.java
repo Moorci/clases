e6d7c487a8fa2e1199101b94263af2e1dcdeefa4
in2dm3-a <in2dm3-a@P5INFA13-16.elorrieta.int>
2025-09-11 08:51:37 +0200

    otra prueba

--- Java/Multiproyecto_PSP/src/ejercicio1/ejer3.java
+++ Java/Multiproyecto_PSP/src/ejercicio1/ejer3.java
@@ -8,22 +8,22 @@ import java.io.InputStreamReader;
 public class ejer3 {
 
 	public static void main(String[] args) {
-        ProcessBuilder pb = new ProcessBuilder("cmd", "/C", "getmac");
-        try {
-            Process proces = pb.start();
-            InputStream is = proces.getInputStream();
-            BufferedReader bis = new BufferedReader(new InputStreamReader(is));
-            String linea = "";
-            while ((linea = bis.readLine()) != null) {
-                linea = linea.trim();//quita espacios
-                if (linea.contains("-")) {//si la linea contiene un guion, es decir, una direc.mac
-                    String mac = linea.split(" ")[0];//divide hasta el espacio
-                    System.out.println(mac);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		ProcessBuilder pb = new ProcessBuilder("cmd", "/C", "getmac");
+		try {
+			Process proces = pb.start();
+			InputStream is = proces.getInputStream();
+			BufferedReader bis = new BufferedReader(new InputStreamReader(is));
+			String linea = "";
+			while ((linea = bis.readLine()) != null) {
+				linea = linea.trim();//quita espacios
+				if (linea.contains("-")) {//si la linea contiene un guion, es decir, una direc.mac
+					String mac = linea.split(" ")[0];//divide hasta el espacio
+					System.out.println(mac);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
