59c08ca4ca80d6f0210fd7310f191db8f04f9691
morci <morci@Alvaro>
2025-09-21 09:55:40 +0200

    Ejercicio DOM terminado

--- EjercicioDOM_1_9/src/DOM/Main.java
+++ EjercicioDOM_1_9/src/DOM/Main.java
@@ -1,10 +1,12 @@
 package DOM;
 
 import java.io.File;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -22,6 +24,15 @@ public class Main {
 			Document doc = dBuilder.parse(archivo);
 			doc.getDocumentElement().normalize();
 			NodeList nList = doc.getElementsByTagName("CD");
+			
+			agregarCD(doc, "X100PRE", "Bad Bunny", "PR", "Sony", "12.99", "2018");
+
+			// Guardar el documento actualizado en el archivo XML
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(archivo);
+			transformer.transform(source, result);
 
 			for (int i = 0; i < nList.getLength(); i++) {
 				Node nNode = nList.item(i);
@@ -44,5 +55,36 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void agregarCD(Document doc, String title, String artist, String country, String company, String price, String year) {
+	    Element nuevoCD = doc.createElement("CD");
+
+	    Element titulo = doc.createElement("TITLE");
+	    titulo.setTextContent(title);
+	    nuevoCD.appendChild(titulo);
+
+	    Element artista = doc.createElement("ARTIST");
+	    artista.setTextContent(artist);
+	    nuevoCD.appendChild(artista);
+
+	    Element pais = doc.createElement("COUNTRY");
+	    pais.setTextContent(country);
+	    nuevoCD.appendChild(pais);
+
+	    Element sello = doc.createElement("COMPANY");
+	    sello.setTextContent(company);
+	    nuevoCD.appendChild(sello);
+
+	    Element precio = doc.createElement("PRICE");
+	    precio.setTextContent(price);
+	    nuevoCD.appendChild(precio);
+
+	    Element anio = doc.createElement("YEAR");
+	    anio.setTextContent(year);
+	    nuevoCD.appendChild(anio);
+
+	    doc.getDocumentElement().appendChild(nuevoCD);
+	}
+	
 
 }
