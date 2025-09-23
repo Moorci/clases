0ded09e3111e83a58676baf6183dc8f96f7f3577
morci <morci@Alvaro>
2025-09-23 22:51:12 +0200

    terminado ejercicio xpath

--- EjercicioXPath/src/ejercicio/main.java
+++ EjercicioXPath/src/ejercicio/main.java
@@ -1,7 +1,6 @@
 package ejercicio;
 
 import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,41 +15,50 @@ import org.xml.sax.SAXException;
 
 public class main {
 
-	public static void main(String[] args) {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db;
-		try {
-			db = dbf.newDocumentBuilder();
-		
-		Document xpathDoc = db.parse("ventas.xml");
-		XPath xpath = XPathFactory.newInstance().newXPath();
-		
-	
-		String expresion = "count(/tienda/ventas/venta[producto = /tienda/productos/producto[@venta = /tienda/dptos/dpto[nombre='Carnicería']/@id]/@id])\r\n";
-		String expresion2 = "/tienda/productos/producto[@venta = /tienda/dptos/dpto[nombre='Carnicería']/@id]/precio\r\n";
-		String expresion3 = "/tienda/productos/producto[@id = /tienda/ventas/venta[cantidad=3]/producto]/nombre\r\n";
-		String expresion4 = "/tienda/dptos/dpto[@id = /tienda/productos/producto[nombre='Naranjas']/@venta]/responsable";
-		String expresion5 = "/tienda/dptos/dpto[@id = /tienda/productos/producto[@id = /tienda/ventas/venta[data='2013/3/10']/producto]/@venta]/responsable\r\n";
-		//String expresion = "//book[author = 'Neal Stephenson']/ title";
-		
-		NodeList ventas = (NodeList) xpath.evaluate(expresion,  xpathDoc,XPathConstants.NODESET);
-		NodeList ventas2 = (NodeList) xpath.evaluate(expresion2,  xpathDoc,XPathConstants.NODESET);
-		NodeList ventas3 = (NodeList) xpath.evaluate(expresion3,  xpathDoc,XPathConstants.NODESET);
-		NodeList ventas4 = (NodeList) xpath.evaluate(expresion4,  xpathDoc,XPathConstants.NODESET);
-		NodeList ventas5 = (NodeList) xpath.evaluate(expresion5,  xpathDoc,XPathConstants.NODESET);
-		
-		for(int i =0;i<libros.getLength();i++)
-		{
-			System.out.println(libros.item(i).getNodeValue());
-		}
-	
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (XPathExpressionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
-	}
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document xpathDoc = db.parse("ventas.xml");
+            XPath xpath = XPathFactory.newInstance().newXPath();
+
+            String expresion1 = "count(/tienda/ventas/venta[producto = /tienda/productos/producto[@venta = /tienda/dptos/dpto[nombre='Carnicería']/@id]/@id])";
+            String expresion2 = "/tienda/productos/producto[@venta = /tienda/dptos/dpto[nombre='Carnicería']/@id]/precio";
+            String expresion3 = "/tienda/productos/producto[@id = /tienda/ventas/venta[cantidad=3]/producto]/nombre";
+            String expresion4 = "/tienda/dptos/dpto[@id = /tienda/productos/producto[nombre='Naranjas']/@venta]/responsable";
+            String expresion5 = "/tienda/dptos/dpto[@id = /tienda/productos/producto[@id = /tienda/ventas/venta[data='2013/3/10']/producto]/@venta]/responsable";
+
+            Double countVentas = (Double) xpath.evaluate(expresion1, xpathDoc, XPathConstants.NUMBER);
+            System.out.println("Cantidad de ventas en Carnicería: " + countVentas);
+
+            NodeList preciosCarniceria = (NodeList) xpath.evaluate(expresion2, xpathDoc, XPathConstants.NODESET);
+            NodeList nombresCantidad3 = (NodeList) xpath.evaluate(expresion3, xpathDoc, XPathConstants.NODESET);
+            NodeList responsableNaranjas = (NodeList) xpath.evaluate(expresion4, xpathDoc, XPathConstants.NODESET);
+            NodeList responsableFecha = (NodeList) xpath.evaluate(expresion5, xpathDoc, XPathConstants.NODESET);
+
+            System.out.println("\nPrecios de productos en Carnicería:");
+            for (int i = 0; i < preciosCarniceria.getLength(); i++) {
+                System.out.println(preciosCarniceria.item(i).getTextContent());
+            }
+
+            System.out.println("\nNombre de productos vendidos con cantidad = 3:");
+            for (int i = 0; i < nombresCantidad3.getLength(); i++) {
+                System.out.println(nombresCantidad3.item(i).getTextContent());
+            }
+
+            System.out.println("\nResponsable del departamento que vende Naranjas:");
+            for (int i = 0; i < responsableNaranjas.getLength(); i++) {
+                System.out.println(responsableNaranjas.item(i).getTextContent());
+            }
+
+            System.out.println("\nResponsable del departamento de venta del 2013/3/10:");
+            for (int i = 0; i < responsableFecha.getLength(); i++) {
+                System.out.println(responsableFecha.item(i).getTextContent());
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
+            e.printStackTrace();
+        }
+    }
 }
